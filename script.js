7f50110ca978ca3f9425aa7c406a4b389df653f3
vinosia-42 <mr.vinosia@gmail.com>
2026-06-29 12:29:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function number(){   
+    let userinput = prompt("====input a number====")
+    for(let i = 0; i<=userinput; i++){
+        if(i % 2 == 0){
+            document.writeln("<div class =score>"+ i+"</div>")
+        }
+    }
+    x = Math.sqrt();
+ 
+}
+number()
+alert("done")
